038c096b1208d95e71ecbf88da7d6cdbbe865ad2
MuliJob <munyoki912@gmail.com>
2025-06-26 14:01:23 +0300

    :sparkles: Added nav-bar component and home page

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="navbar bg-base-100 shadow-sm">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">Moti</a>
+    </div>
+    <div class="flex-none">
+      <button class="btn btn-dash btn-primary">
+        Login
+        <Icon
+          name="ic:outline-login"
+          class="text-lg"
+        />
+      </button>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <img
+        src="https://s.alicdn.com/@sc04/kf/Hb57aa11ae16340c2a2134dbed854590bK.jpg_300x300.jpg"
+        class="max-w-sm rounded-lg shadow-2xl"
+      >
+      <div>
+        <h1 class="text-5xl font-bold">
+          Buy & Sell Auto Spare Parts with Moti
+        </h1>
+        <p class="py-6">
+          Find quality spare parts for all vehicle makes and models — or list your own! Whether you're a car owner, mechanic, or reseller, our platform connects you to the right parts at the right price.
+        </p>
+
+        <button class="btn btn-dash btn-primary">
+          Get Started
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
