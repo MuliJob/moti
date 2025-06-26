7ede1100e4f788cf6ed2ca0aa02f7f8edf2125a0
MuliJob <munyoki912@gmail.com>
2025-06-26 15:36:51 +0300

    :sparkles: Created component to toggle themes

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="navbar bg-base-100 shadow-sm">
     <div class="flex-1">
-      <a class="btn btn-ghost text-xl">Moti</a>
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Moti
+      </NuxtLink>
     </div>
     <div class="flex-none">
-      <button class="btn btn-dash btn-primary">
+      <AppThemeToggle />
+      <button class="btn btn-outline btn-primary">
         Login
         <Icon
           name="ic:outline-login"
-          class="text-lg"
+          size="24"
         />
       </button>
     </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "@nuxtjs/color-mode",
+  ],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
@@ -14,11 +22,7 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
-  css: ["~/assets/css/main.css"],
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/ui",
-    "@nuxt/icon",
-    "@nuxt/fonts",
-  ],
+  colorMode: {
+    dataValue: "theme",
+  },
 });
