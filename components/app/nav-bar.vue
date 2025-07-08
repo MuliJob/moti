c86caf22bf4ef4484bd71f32809129db024d9ade
MuliJob <munyoki912@gmail.com>
2025-07-08 14:59:03 +0300

    :sparkles: made the navbar fixed at the top

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
+  <div class="navbar bg-base-100 shadow-sm fixed top-0 z-50">
     <div class="flex-1">
       <NuxtLink to="/" class="btn btn-ghost text-xl">
         Moti
