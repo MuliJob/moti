4caf852c797b4c4e2a3f2be14aac54baf72468f5
MuliJob <munyoki912@gmail.com>
2025-07-09 21:46:00 +0300

    :sparkles: Added link for users if no account already exists

--- pages/login.vue
+++ pages/login.vue
@@ -16,7 +16,7 @@
             class="input input-bordered w-full"
             placeholder="Enter your email"
             required
-          >
+          />
         </div>
 
         <div>
@@ -30,12 +30,12 @@
             placeholder="Enter your password"
             required
             minlength="6"
-          >
+          />
         </div>
 
         <div class="flex items-center justify-between text-sm mt-2">
           <label class="flex items-center gap-2 cursor-pointer">
-            <input type="checkbox" class="checkbox checkbox-sm">
+            <input type="checkbox" class="checkbox checkbox-sm" />
             <span class="label-text">Remember me</span>
           </label>
           <a href="#" class="link link-hover text-primary">Forgot password?</a>
@@ -48,20 +48,34 @@
     </form>
 
     <!-- Divider -->
-    <div class="divider my-6">
-      Or
-    </div>
+    <div class="divider my-6">Or</div>
 
     <!-- Social login buttons -->
     <div class="flex flex-col gap-3">
-      <button class="btn btn-outline w-full">
-        <img src="https://www.svgrepo.com/show/475656/google-color.svg" class="w-5 h-5 mr-2">
+      <button class="btn btn-outline w-full flex items-center justify-center">
+        <img
+          src="https://www.svgrepo.com/show/475656/google-color.svg"
+          class="w-5 h-5 mr-2"
+          alt="Google"
+        />
         Continue with Google
       </button>
-      <button class="btn btn-outline w-full">
-        <img src="https://www.svgrepo.com/show/475654/github-color.svg" class="w-5 h-5 mr-2">
+      <button class="btn btn-outline w-full flex items-center justify-center">
+        <img
+          src="https://www.svgrepo.com/show/475654/github-color.svg"
+          class="w-5 h-5 mr-2"
+          alt="GitHub"
+        />
         Continue with GitHub
       </button>
     </div>
+
+    <!-- Register link -->
+    <p class="text-center text-sm text-gray-600 mt-6">
+      Don't have an account?
+      <a href="/register" class="text-primary font-semibold hover:underline">
+        Register here
+      </a>
+    </p>
   </div>
 </template>
